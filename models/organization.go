6cdb98a1f2d680e524070bf8e7b8e93965595744
kaitobq <kaitobq520@gmail.com>
2024-07-26 20:16:32 +0900

    refactor: フィールド名の統一 feat: db周りの処理

--- models/organization.go
+++ models/organization.go
@@ -1,7 +1,41 @@
 package models
 
+import "fmt"
+
 type Organization struct {
-    OrganizationID   int    `json:"organization_id"`
-    OrganizationName string `json:"organization_name"`
-    TotalContributions       int    `json:"total_contributions"`
+    Id                 int    `json:"id"`
+    Name               string `json:"name"`
+    TotalContributions int    `json:"total_contributions"`
+}
+
+func (o *Organization) CreateNewRecord() error { // contributionの取得は行わない
+    var count int
+    err := DB.QueryRow("SELECT COUNT(*) FROM organizations WHERE id = ?", o.Id).Scan(&count)
+    if err != nil {
+        return fmt.Errorf("%w: %v", Err005, err)
+    }
+    if count > 0 {
+        return fmt.Errorf("%w: %v", Err005, "organization already exists")
+    }
+
+    result, err := DB.Exec("INSERT INTO organizations (name) VALUES (?)", o.Name)
+    if err != nil {
+        return fmt.Errorf("%w: %v", Err005, err)
+    }
+
+    id, err := result.LastInsertId()
+    if err != nil {
+        return fmt.Errorf("%w: %v", Err005, err)
+    }
+    o.Id = int(id)
+
+    return nil
+}
+
+func (o *Organization) UpdateTotalContributions() error {
+    _, err := DB.Exec("UPDATE organizations SET total_contributions = ? WHERE id = ?", o.TotalContributions, o.Id)
+    if err != nil {
+        return fmt.Errorf("%w: %v", Err006, err)
+    }
+    return nil
 }
